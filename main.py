3e3a78a92ee1a125552a6c3d6901535680f4e2be
Anh Truong <atruon1995@gmail.com>
2022-04-07 00:12:43 -0700

    Added more comments and separated the display and main function to main.py

---
+++ main.py
@@ -0,0 +1,53 @@
+from weight_calculator import Weight
+
+def display(forty_five: int, thirty_five: int, twenty_five: int, ten: int, five: int , two_half: int):
+    """
+    Parameters: - the quantity of 45lb plate
+                - the quantity of 35lb plate
+                - the quantity of 25lb plate
+                - the quantity of 10lb plate
+                - the quantity of 5lb plate
+                - the quantity of 2.5lb plate
+    
+    Return: None
+    
+    Description: This function displays the quantity of each plate (if the quantity is not zero)
+    that the user needs for both sides of the bar
+    """
+
+    # Checks the quantity of each plate, if the quantity is not zero, displays the amount
+    if forty_five != 0:
+        print(f"45 plates: {forty_five}")
+    if thirty_five != 0:
+        print(f"35 plates: {thirty_five}")
+    if twenty_five != 0:
+        print(f"25 plates: {twenty_five}")
+    if ten != 0:
+        print(f"10 plates: {ten}")
+    if five != 0:
+        print(f"5 plates: {five}")
+    if two_half != 0:
+        print(f"2.5 plates: {two_half}")
+
+def main():
+    weight = Weight()
+    
+    print("-----------------------------------------------------------\n")
+    print("                      Weight Calculator                    ")
+    print("\n-----------------------------------------------------------\n")
+
+    # Checks the user input if they are valid
+    valid_weight, valid_bar_weight = weight.check_valid_input()
+        
+    # If so, calculates the plates
+    weight.calculate_plates(valid_weight, valid_bar_weight)
+
+    # Display the plates
+    print("\n-----------------------------------------------------------\n")
+    print(f"For {valid_weight}lbs, you need: ")
+    display(weight.forty_five, weight.thirty_five, weight.twenty_five, \
+    weight.ten,weight.five,weight.two_half)
+    print("\nThank you for using the program. Bye!\n")
+
+if __name__ == "__main__":
+    main()
--- weight_calculator.py
+++ weight_calculator.py
@@ -1,39 +1,78 @@
 from re import T
-from typing import List, Dict
-from collections import defaultdict
 import sys
 
 class Weight:
+    """
+    Stores the amount of each type of plate based on the user input 
+    for a weight and the weight of bar
+    """
     def __init__(self):
+        # The amount of a plate
         self.forty_five = 0
+        # The amount of a 35lb plate
         self.thirty_five = 0
+        # The amount of a 25lb plate
         self.twenty_five = 0
+        # The amount of a 10lb plate
         self.ten = 0
+        # The amount of a 5lb plate
         self.five = 0
+        # The amount of a 2.5lb plate
         self.two_half = 0
 
     def check_valid_input(self):
+        """ 
+        Parameters: None
+
+        Return: A valid weight and the valid weight of the bar
+
+        Description: The method checks user input for a weight and the weight of bar 
+        """
         weight = ""
         bar = ""
 
+        # Check the validity of the weight
         while True:
             weight = input("Please enter a weight that is divisible by 5 and greater than the bar (lbs): ")
+
+            """ If the input is all digit, greater than 5 
+            (the minimum weight for a pair of the smallest plate, which is 2.5lb),
+            and the weight is divisible by 5, then convert the user input to integer and exit the loop. 
+            Otherwise, continue asking the user to enter a valid weight.
+            """
             if weight.isdigit() and int(weight) >= 5 and int(weight) % 5 == 0:
                 weight = int(weight)
                 break
-
+        
+        # Check the validity of the weight of the bar 
         while True:
             bar_input = input("Do you want to enter a different weight for the bar (default is 45lbs)? ")
+
+            """ If input is all digit, greater than 0, less than the weight, 
+            and is divisible by 5, then convert the input to integer and exit the loop"""
             if bar_input.isdigit() and int(bar_input) > 0 and int(bar_input) < weight and int(bar_input) % 5 == 0: 
                 bar = int(bar_input)
                 break
+
+            """ If the input is blank, then set the bar to 45lbs as the default and exit the loop """
             if bar_input == "":
                 bar = 45
                 break 
 
         return weight, bar
 
-    def calculate_plates(self, weight, bar):
+    def calculate_plates(self, weight: int, bar: int):
+        """
+        Parameters:  The weight to calculate, the weight of the bar
+
+        Return: None
+
+        Description: The method calculates the amount of each type of plates 
+        on the weight and the weight of bar
+
+        """
+
+        # Initializes the size (lbs) of each type of plate
         forty_five_pounds = 45
         thirty_five_pounds = 35
         twenty_five_pounds = 25
@@ -41,79 +80,76 @@ class Weight:
         five_pounds = 5
         two_half_pounds = 2.5 
 
-        #minus the weight of the bar
+        # Minuses the weight of the bar from the weight
         weight -= bar
 
-        #calculate plates from the largest to the smallest plate
+        # Calculates plates from the largest to the smallest plate
         if weight >= 90:
+            """Gets the quantity of the 45lb plate as a pair, so checks if the weight is
+            greater than or equal to 90 (45*2) """
             self.forty_five = weight // forty_five_pounds
+            """ If the quantity is odd, decement the quantity by 1 
+            because you need an even amount to put in both sides of the bar """
             if self.forty_five % 2 == 1:
                 self.forty_five -= 1
+            # Gets the remain quantity of the weight after minusing the quantity of the 45lb
             weight -= (forty_five_pounds * self.forty_five)
 
+        
         if weight >= 70:
+            """Gets the quantity of the 35lb plate as a pair, so checks if the weight is
+            greater than or equal to 70 (35*2)"""
             self.thirty_five = weight // thirty_five_pounds
+            """ If the quantity is odd, decement the quantity by 1 
+            because you need an even amount to put in both sides of the bar """
             if self.thirty_five % 2 == 1:
                 self.thirty_five -= 1
+            # Gets the remain quantity of the weight after minusing the quantity of the 35lb
             weight -= (thirty_five_pounds * self.thirty_five) 
 
+        
         if weight >= 50:
+            """Gets the quantity of the 25lb plate as a pair, so checks if the weight is
+            greater than or equal to 50 (25*2)"""
             self.twenty_five = weight // twenty_five_pounds
+            """ If the quantity is odd, decement the quantity by 1 
+            because you need an even amount to put in both sides of the bar """
             if self.twenty_five % 2 == 1:
                 self.twenty_five -= 1
+            # Gets the remain quantity of the weight after minusing the quantity of the 25lb
             weight -= (twenty_five_pounds * self.twenty_five)
 
+        
         if weight >= 20:
+            """Gets the quantity of the 10lb plate as a pair, so checks if the weight is
+            greater than or equal to 20 (10*2)"""
             self.ten = weight // ten_pounds
+            """ If the quantity is odd, decement the quantity by 1 
+            because you need an even amount to put in both sides of the bar """
             if self.ten % 2 == 1:
                 self.ten -= 1
+            # Gets the remain quantity of the weight after minusing the quantity of the 10lb
             weight -= (ten_pounds * self.ten)
 
+        
         if weight >= 10:
+            """Gets the quantity of the 5lb plate as a pair, so checks if the weight is
+            greater than or equal to 10 (5*2)"""
             self.five = weight // five_pounds
+            """ If the quantity is odd, decement the quantity by 1 
+            because you need an even amount to put in both sides of the bar """
             if self.five % 2 == 1:
                 self.five -= 1
+            # Gets the remain quantity of the weight after minusing the quantity of the 5lb
             weight -= (five_pounds * self.five)
 
         else:
+            """Gets the quantity of the 2.5lb plate as a pair, so checks if the weight is
+            greater than or equal to 5 (2.5*2)"""
             self.two_half = int(weight // two_half_pounds)
+            """ If the quantity is odd, decement the quantity by 1 
+            because you need an even amount to put in both sides of the bar """
             if self.two_half % 2 == 1:
                 self.two_half -= 1
+            # Gets the remain quantity of the weight after minusing the quantity of the 2.5lb
             weight -= (two_half_pounds * self.two_half)
-
-def display(forty_five,thirty_five,twenty_five,ten,five,two_half):
-    if forty_five != 0:
-        print(f"45 plates: {forty_five}")
-    if thirty_five != 0:
-        print(f"35 plates: {thirty_five}")
-    if twenty_five != 0:
-        print(f"25 plates: {twenty_five}")
-    if ten != 0:
-        print(f"10 plates: {ten}")
-    if five != 0:
-        print(f"5 plates: {five}")
-    if two_half != 0:
-        print(f"2.5 plates: {two_half}")
-
-if __name__ == "__main__":
-    weight = Weight()
-    
-    print("-----------------------------------------------------------\n")
-    print("                      Weight Calculator                    ")
-    print("\n-----------------------------------------------------------\n")
-
-    # check the user input if it is valid
-    valid_weight, valid_bar_weight = weight.check_valid_input()
-        
-    # if so, calculate the plates
-    weight.calculate_plates(valid_weight, valid_bar_weight)
-
-    # display the plates
-    print("\n-----------------------------------------------------------\n")
-    print(f"For {valid_weight}lbs, you need: ")
-    display(weight.forty_five, weight.thirty_five, weight.twenty_five, \
-    weight.ten,weight.five,weight.two_half)
-    print("\nThank you for using the program. Bye!\n")
-
-
-
