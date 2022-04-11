3d11358a775101a73e476c85816e802b0c481ad5
Anh Truong <atruon1995@gmail.com>
2022-04-10 22:04:45 -0700

    Optimized the check methods. Reformatted the style

--- weight_calculator.py
+++ weight_calculator.py
@@ -4,10 +4,11 @@ and calculate the quantity of plates for a specific weight that is determined by
 """
 
 class WeightCalculator:
+    """Stores the amount of each type of plate based on the user input
+     for a weight and the weight of bar
     """
-    Stores the amount of each type of plate based on the user input 
-    for a weight and the weight of bar
-    """
+    DEFAULT_BAR_WEIGHT = 45
+
     def __init__(self):
         # The amount of a plate
         self.forty_five = 0
@@ -22,37 +23,36 @@ class WeightCalculator:
         # The amount of a 2.5lb plate
         self.two_half = 0
 
-    def check_valid_weight(self, w_input: str) -> bool:
+    def check_valid_weight(self, w_input: str, b_input: int) -> bool:
         """ The function checks if the weight input from the user is valid.
 
         :param w_input: the weight input from the user
         :type w_input: str
-        :return: Return True if the weight input is digit, greater than 5 
+        :param b_input: the valid weight of the bar
+        :type b_input: int
+        :return: Return True if the weight input is digit, greater than 5
         (the minimum weight for a pair of the smallest plate, which is 2.5lb),
         and is divisible by 5, else return False
         :rtype: bool
         """
-        # TODO: Check that the bar weight is less than the weight input
-        return w_input.isdigit() and int(w_input) >= 5 and int(w_input) % 5 == 0
+        return w_input.isdigit() and int(w_input) >= 5 \
+            and int(w_input) % 5 == 0 and int(w_input) > b_input
 
-    def check_valid_bar(self, b_input: str, w_input: int) -> bool:
-        """ 
+    def check_valid_bar(self, b_input: str) -> bool:
+        """
         The function checks if the weight of the bar is valid.
 
         :param b_input: the weight of the bar input from the user
         :type b_input: str
-        :param w_input: the valid weight input 
-        :type w_input: int
-        :return: Return True if the weight of the bar is all digit, greater than 0, less than the weight, 
+        :return: Return True if the weight of the bar is all digit, greater than 0,
         is divisible by 5, or is blank. Otherwise, return False
         :rtype: bool
         """
-        # TODO: Move the bar weight and total weight comparison check into the check_valid_weight method and re-organize the logic. 
-        return b_input == "" or (b_input.isdigit() and int(b_input) >= 0 and int(b_input) < w_input and int(b_input) % 5 == 0)
+        return b_input == "" or (b_input.isdigit() and int(b_input) >= 0 and int(b_input) % 5 == 0)
 
-    def calculate_plates(self, weight: int, bar: int) -> None:
+    def calculate_plates(self, weight: int, bar_weight: int) -> None:
         """
-        The method calculates the amount of each type of plates 
+        The method calculates the amount of each type of plates
         on the weight and the weight of bar
 
         :param weight: the valid weight input
@@ -61,86 +61,72 @@ class WeightCalculator:
         :type bar: int
         """
 
-        # TODO: Making these constants actually makes your code less readable
-        # Initializes the size (lbs) of each type of plate
-        forty_five_pounds = 45
-        thirty_five_pounds = 35
-        twenty_five_pounds = 25
-        ten_pounds = 10
-        five_pounds = 5
-        two_half_pounds = 2.5 
-
         # Minuses the weight of the bar from the weight
-        weight -= bar
+        weight -= bar_weight
 
         # Calculates plates from the largest to the smallest plate
         if weight >= 90:
-            # TODO: Use # for inline comments. Reserve using """triple quote comments""" for docstrings.
-            """Gets the quantity of the 45lb plate as a pair, so checks if the weight is
-            greater than or equal to 90 (45*2) """
-            self.forty_five = weight // forty_five_pounds
-            """ If the quantity is odd, decement the quantity by 1 
-            because you need an even amount to put the weight in both sides of the bar """
+            # Gets the quantity of the 45lb plate as a pair, so checks if the weight is
+            # greater than or equal to 90 (45*2)
+            self.forty_five = weight // 45
+            # If the quantity is odd, decement the quantity by 1
+            # because you need an even amount to put the weight in both sides of the bar
             if self.forty_five % 2 == 1:
                 self.forty_five -= 1
             # Gets the remain quantity of the weight after minusing the quantity of the 45lb
-            weight -= (forty_five_pounds * self.forty_five)
+            weight -= (45 * self.forty_five)
 
-        
         if weight >= 70:
-            """Gets the quantity of the 35lb plate as a pair, so checks if the weight is
-            greater than or equal to 70 (35*2)"""
-            self.thirty_five = weight // thirty_five_pounds
-            """ If the quantity is odd, decement the quantity by 1 
-            because you need an even amount to put the weight in both sides of the bar """
+            # Gets the quantity of the 35lb plate as a pair, so checks if the weight is
+            # greater than or equal to 70 (35*2)
+            self.thirty_five = weight // 35
+            # If the quantity is odd, decement the quantity by 1
+            # because you need an even amount to put the weight in both sides of the bar
             if self.thirty_five % 2 == 1:
                 self.thirty_five -= 1
             # Gets the remain quantity of the weight after minusing the quantity of the 35lb
-            weight -= (thirty_five_pounds * self.thirty_five) 
+            weight -= (35 * self.thirty_five)
 
-        
         if weight >= 50:
-            """Gets the quantity of the 25lb plate as a pair, so checks if the weight is
-            greater than or equal to 50 (25*2)"""
-            self.twenty_five = weight // twenty_five_pounds
-            """ If the quantity is odd, decement the quantity by 1 
-            because you need an even amount to put the weight in both sides of the bar """
+            # Gets the quantity of the 25lb plate as a pair, so checks if the weight is
+            # greater than or equal to 50 (25*2)
+            self.twenty_five = weight // 25
+            # If the quantity is odd, decement the quantity by 1
+            # because you need an even amount to put the weight in both sides of the bar
             if self.twenty_five % 2 == 1:
                 self.twenty_five -= 1
             # Gets the remain quantity of the weight after minusing the quantity of the 25lb
-            weight -= (twenty_five_pounds * self.twenty_five)
+            weight -= (25 * self.twenty_five)
 
-        
         if weight >= 20:
-            """Gets the quantity of the 10lb plate as a pair, so checks if the weight is
-            greater than or equal to 20 (10*2)"""
-            self.ten = weight // ten_pounds
-            """ If the quantity is odd, decement the quantity by 1 
-            because you need an even amount to put the weight in both sides of the bar """
+            # Gets the quantity of the 10lb plate as a pair, so checks if the weight is
+            # greater than or equal to 20 (10*2)
+            self.ten = weight // 10
+            # If the quantity is odd, decement the quantity by 1
+            # because you need an even amount to put the weight in both sides of the bar
             if self.ten % 2 == 1:
                 self.ten -= 1
             # Gets the remain quantity of the weight after minusing the quantity of the 10lb
-            weight -= (ten_pounds * self.ten)
+            weight -= (10 * self.ten)
 
-        
         if weight >= 10:
-            """Gets the quantity of the 5lb plate as a pair, so checks if the weight is
-            greater than or equal to 10 (5*2)"""
-            self.five = weight // five_pounds
-            """ If the quantity is odd, decement the quantity by 1 
-            because you need an even amount to put the weight in both sides of the bar """
+            # Gets the quantity of the 5lb plate as a pair, so checks if the weight is
+            # greater than or equal to 10 (5*2)
+            self.five = weight // 5
+            # If the quantity is odd, decement the quantity by 1
+            # because you need an even amount to put the weight in both sides of the bar
             if self.five % 2 == 1:
                 self.five -= 1
             # Gets the remain quantity of the weight after minusing the quantity of the 5lb
-            weight -= (five_pounds * self.five)
+            weight -= (5 * self.five)
 
         if weight >= 5:
-            """Gets the quantity of the 2.5lb plate as a pair, so checks if the weight is
-            greater than or equal to 5 (2.5*2)"""
-            self.two_half = int(weight // two_half_pounds)
-            """ If the quantity is odd, decement the quantity by 1 
-            because you need an even amount to put the weight in both sides of the bar """
+            # Gets the quantity of the 2.5lb plate as a pair, so checks if the weight is
+            # greater than or equal to 5 (2.5*2)
+            self.two_half = int(weight // 2.5)
+            # If the quantity is odd, decement the quantity by 1
+            # because you need an even amount to put the weight in both sides of the bar
             if self.two_half % 2 == 1:
                 self.two_half -= 1
             # Gets the remain quantity of the weight after minusing the quantity of the 2.5lb
-            weight -= (two_half_pounds * self.two_half)
+            weight -= (2.5 * self.two_half)
